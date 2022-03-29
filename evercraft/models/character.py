b9c549a1e0b81a9a30b12a223318b742980c8b92
Anthony DeVarti <anthony.devarti@student.awesomeinc.org>
2022-03-29 19:01:56 +0000

    got the setters set up correclty

--- evercraft/models/character.py
+++ evercraft/models/character.py
@@ -1,3 +1,27 @@
 # this is where your character code will go
+traits = {
+    "name": "Rufus",
+    "alignment": 'good',
+    "AC": 10
+    }
+class Character:
+    
+    def __init__(self, object):
+        self.name = object["name"]
+        self.alignment = object["alignment"]
+        self.AC = object["AC"]
+    
+    def set_name(self, name, alignment):
+       self.name = name
 
-        
+    def get_name(self):
+       return self.name
+
+
+c1 = Character(traits)
+# c2 = Character("Bob", "evil")
+
+print(c1.AC)
+
+c2 = Character(traits)
+print(c2.AC)
--- tests/test_iteration1.py
+++ tests/test_iteration1.py
@@ -1,1 +1,52 @@
 import pytest
+from evercraft.models.character import Character
+#### Feature: Create a Character
+#get a name
+#set a name
+
+#make sure that a character instance can be made
+# def test_makeCharacter():
+#     assert Character()
+
+
+# default test case for character class
+def test_setCharacterName():
+    ret_name = 'Evercraft'
+    traits = {
+    "name": "Evercraft",
+    "alignment": 'good',
+    "AC": 10
+    }
+    c1 = Character(traits)
+    assert c1.name == ret_name 
+
+# test case for character name
+def test_getCharacterName():
+    ret_name = "Rufus"
+    traits = {
+    "name": "Rufus",
+    "alignment": 'good',
+    "AC": 10
+    }
+    c1 = Character(traits)
+    assert c1.get_name()==ret_name
+
+# make character alignment good
+def test_makeCharacterAlignGood():
+    align_value = 'good'
+    traits = {
+    "name": "Rufus",
+    "alignment": 'good',
+    "AC": "10"
+    }
+    c1 = Character(traits)
+    assert c1.alignment == align_value
+
+# checking for default values
+#if an empty object is passed in, will the AC==10?
+# this assumes that the object being passed in is empty, so we can check for
+# a default value rather than manually passing one in at creation
+def test_defaultAC():
+    traits= {}
+    c2 = Character(traits)
+    assert c2.AC==10
