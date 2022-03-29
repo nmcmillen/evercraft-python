2a2affb879f0aec96f61c6bcf5f4666ad7a069c5
Anthony DeVarti <anthony.devarti@student.awesomeinc.org>
2022-03-29 21:11:42 +0000

    loop attempt on it one

--- evercraft/models/character.py
+++ evercraft/models/character.py
@@ -1,14 +1,26 @@
 # this is where your character code will go
-traits = {
+TRAITS = {
     "name": "Rufus",
     "alignment": 'good',
-    "AC": 10
+    "AC": 10,
+    'hit_points': 5
     }
+
+CHARACTER_DEF = {
+    "name": "Evercraft",
+    "alignment": "neutral",
+    "AC": 10,
+    "hit_point": 5
+}
+
 class Character:
-    
     def __init__(self, object):
-        self.name = object["name"]
-        self.alignment = object["alignment"]
+        if self.name in TRAITS.keys():
+            TRAITS["name"] = self.values.name
+        else:
+            CHARACTER_DEF["name"] = self.values.name
+        self.name = object["name"], default["name"]
+        self.alignment = object["alignment"] 
         self.AC = object["AC"]
     
     def set_name(self, name, alignment):
@@ -18,10 +30,8 @@ class Character:
        return self.name
 
 
-c1 = Character(traits)
-# c2 = Character("Bob", "evil")
+c1 = Character(TRAITS, CHARACTER_DEF)
+#c2 = Character("Bob", "evil")
 
-print(c1.AC)
+print(c1.AC) 
 
-c2 = Character(traits)
-print(c2.AC)
